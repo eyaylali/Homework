2e679ea8659c262836ed19c9f0c715429ff58f16
Erin Yaylali <erinyaylali@gmail.com>
2015-01-13 23:12:35 -0800

    Corrected errors and refactored code

--- Homework02/produce_summary.py
+++ Homework02/produce_summary.py
@@ -1,47 +1,25 @@
+def print_output(csv):
+    my_file = open(csv)
+    for line in my_file:
+        line = line.rstrip()
+        words = line.split(',')
+    
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
+    
+        print "Delivered %s %ss for a total of: $%s" % (count, melon, amount)
+    my_file.close()
+
 # Day 1
 print "Day 1"
-my_file = open("um-deliveries-20140519.csv")
-
-for line in my_file:
-    line = line.rstrip()
-    words = line.split(',')
-    
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-    
-    print "Delivered %s %ss for a total of: $%s" % (count, melon, amount)
-my_file.close()
-print
+print_output("um-deliveries-20140519.csv")
 
 # Day 2
 print "Day 2"
-my_file = open("um-deliveries-20140520.csv")
-
-for line in my_file:
-    line = line.rstrip()
-    words = line.split(',')
-    
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-    
-    print "Delivered %s %ss for a total of: $%s" % (count, melon, amount)
-my_file.close()
-print
+print_output("um-deliveries-20140520.csv")
 
 # Day 3
 print "Day 3"
-my_file = open("um-deliveries-20140521.csv")
+print_output("um-deliveries-20140521.csv")
 
-for line in my_file:
-    line = line.rstrip()
-    words = line.split(',')
-    
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-    
-    print "Delivered %s %ss for a total of: $%s" % (count, melon, amount)
-my_file.close()
-print
